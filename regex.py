2713ab2f64da402d5fce5995afb1c038cb702c28
LucasAMoreira <39498462+LucasAMoreira@users.noreply.github.com>
2021-10-04 15:11:31 -0300

    Adição de mais expressões regulares

--- regex.py
+++ regex.py
@@ -20,14 +20,13 @@ tokens=[
 	'NUM',
 	'RELOP',
 	'ARIOP', # Operador aritmético
-	'COMENT',
+	'COMMENT',
 	'ATRIBUITION',
-	'COLON',
+	'COMMA',
 	'SEMICOLON',
-	'PARENTHESIS'
-	'COLCHETES',
-	'CHAVE',
-	
+	'PARENTHESES',
+	'BRACKETS', #colchetes
+	'BRACES',# chave	
 ];
 
 # Tokens são a lista anterior e as palavras reservadas.
@@ -42,32 +41,30 @@ letra = r'([a-zA-Z])'
 t_NUM          = r'('+digito + r'+' + digito +r'*)'
 t_RELOP        = r'< | <= | > | >= | == | !='
 t_ARIOP        = r'\+ | \- | \* | /'
-
-#t_COMENT
-
 t_ATRIBUITION  = r'='
-t_COLON        = r','
+t_COMMA        = r','
 t_SEMICOLON    = r';'
+t_PARENTHESES  = r'\( | \)'
+t_BRACKETS = r'\[ | \]'
+t_BRACES = r'\{ | \}'
 
-'''  
-t_PARENTHESIS  = r'(\()'
-t_COLCHETES = r'\[ | \]'
-t_CHAVE = r'\{ | \}'
-'''
 
 def t_ID(t):
 	r'([a-zA-Z] + [a-zA-Z]*)'
 	t.type = reservadas.get(t.value,'ID')
 	return t;
 
-
+def t_COMMENT(t):
+	r'/\*.*\*/'
+	pass
 
 ######################################################################################
 #				TESTES
 ######################################################################################
 
 # A string containing ignored characters (spaces and tabs)
-t_ignore  = ' \t'
+t_ignore  = ' \t \n'
+
 
 # Error handling rule
 def t_error(t):
@@ -78,18 +75,22 @@ def t_error(t):
 lexer = lex.lex();
 
 # Test it out
-data =  'var=5; /*COMENTÁRIO*/ \nif(var>5)\n\tvar = var + 1;'
+data =  'var=5; /*\'COMENTÁRIO\'*/ \nif(var>5){\n\tvar = var + 1;\n}'
 
 print(data)
  # Give the lexer some input
 lexer.input(data)
  
- # Tokenize
+# Tokenize
+i=0
 while True:
      tok = lexer.token()
      if not tok: 
          break      # No more input
      print(tok)
+     i=i+1
+     
+print(i)
 
 
 
