994aea6e733139dd1a1679a26c92f365d408ac79
LucasAMoreira <39498462+LucasAMoreira@users.noreply.github.com>
2021-12-22 10:20:55 -0300

    Invocação com main
    Para usar o compilador executar main.py

--- glc.py
+++ glc.py
@@ -1,10 +1,7 @@
 import ply.yacc as yacc
 import ply.lex as lex
-# from regex import tokens
 import regex
-# from AST import *
-import warnings
-import sys
+
 
 precedence = (
     ('left','PLUS','MINUS'),
@@ -13,12 +10,8 @@ precedence = (
 
 def p_programa(p):
 	'programa : declaracao_lista'
-	#p[0] = p[1]
 	p[0] = ('programa',p[1])
-	global raiz
-	raiz = p[0]
-	
- 
+
 def p_declaracao_lista(p):
 	'''
 	declaracao_lista : declaracao_lista declaracao
@@ -42,18 +35,11 @@ def p_declaracao(p):
 def p_var_declaracao(p):
 	'''
 	var_declaracao : tipo_especificador ID SEMICOLON	
-		| tipo_especificador ID OBT NUM CBT SEMICOLON
+				| tipo_especificador ID OBT NUM CBT SEMICOLON
 	'''
-	x=[]
-	i=1
-	while(i<len(p)):
-		x.append(p[i])
-		i=i+1 
 	if len(p) == 4:
-		p[0] = x #p[1] + p[2] + p[3]
 		p[0] = ('var-declaração',p[1],p[2],p[3])
 	else:
-		p[0] = x #p[1] + p[2] + p[3] + p[4] + p[5] + p[6]
 		p[0] = ('var-declaração',p[1],p[2],p[3])
   
 def p_tipo_especificador(p):
@@ -61,22 +47,12 @@ def p_tipo_especificador(p):
 	tipo_especificador : INT
 				    | VOID
 	'''
-	p[0] = p[1]
 	p[0] = ('tipo-especificador',p[1])
   
 def p_fun_declaracao(p):
 	'''
 	fun_declaracao : tipo_especificador ID OP params CP composto_decl
 	'''
-	x=[]
-	x.append(p[1])
-	x.append(p[2])
-	x.append(p[3])
-	x.append(p[4])
-	x.append(p[5])
-	x.append(p[6])	
-	p[0] = x
-	#p[0] = p[1] + p[2] + p[3] + p[4] + p[5] + p[6]
 	p[0] = ('fun-declaração', p[1], p[2], p[3], p[4], p[5], p[6])
  
 def p_params(p):
@@ -84,25 +60,16 @@ def p_params(p):
 	params : param_lista
 		  | VOID
 	'''
-	p[0] = p[1]
 	p[0] = ('params',p[1])
 
 def p_param_lista(p):
 	'''
 	param_lista : param_lista COMMA param
 			  | param
-	'''
-	x=[]
-	i=1
-	while(i<len(p)):
-		x.append(p[i])
-		i=i+1 
-	
+	'''	
 	if len(p) == 4:
-		p[0] = x #p[1] + p[2] + p[3]
 		p[0] = ('param-lista', p[1], p[2], p[3])
 	else:
-		p[0] = x #p[1]
 		p[0] = ('param-lista', p[1])
  
 def p_param(p):
@@ -110,34 +77,15 @@ def p_param(p):
 	param : tipo_especificador ID 
 		 | tipo_especificador ID OBT CBT
 	'''
-	x=[]
-	i=1
-	while(i<len(p)):
-		x.append(p[i])
-		i=i+1 
-		
 	if len(p) == 3:
-		p[0] = x #p[1] + p[2]
 		p[0] = ('param', p[1], p[2])
 	else:
-		p[0] = x #p[1] + p[2] + p[3] + p[4]
 		p[0] = ('param', p[1], p[2], p[3], p[4])
   
 def p_composto_decl(p):
 	'''
 	composto_decl : OBR local_declaracoes statement_lista CBR
 	'''
-	#	print("########################################################")
-	#	print(p[1])
-	#	print(p[2])
-	#	print(p[3])
-	#	print(p[4])
-	x=[]
-	x.append(p[1])
-	x.append(p[2])
-	x.append(p[3])
-	x.append(p[4])
-	p[0] = x
 	p[0] = ('composto-decl', p[1], p[2], p[3], p[4])
 	
 	
@@ -159,11 +107,6 @@ def p_statement_lista(p):
 				 | vazio
 	'''
 	if len(p) == 3:
-		x = []
-		x.append(p[1])
-		x.append(p[2])
-		p[0]=x	
-		#p[0] = p[1] + p[2]
 		p[0] = ('statement-lista', p[1], p[2])
 	else:
 		p[0] = p[1]
@@ -177,7 +120,6 @@ def p_statement(p):
 			| iteracao_decl 
 			| retorno_decl
 	'''
-	p[0] = p[1]
 	p[0] = ('statement', p[1])
  
 def p_expressao_decl(p):
@@ -185,17 +127,9 @@ def p_expressao_decl(p):
 	expressao_decl : expressao SEMICOLON
 				| SEMICOLON
 	'''
-	x=[]
-	i=1
-	while(i<len(p)):
-		x.append(p[i])
-		i=i+1 
-
 	if len(p) == 3:
-		p[0] = x #p[1] + p[2]
 		p[0] = ('expressão-decl',p[1], p[2])
 	else:
-		p[0] = x #p[1]
 		p[0] = ('expressão-decl',p[1])
   
 def p_selecao_decl(p):
@@ -203,16 +137,9 @@ def p_selecao_decl(p):
 	selecao_decl : IF OP expressao CP statement
 			   | IF OP expressao CP statement ELSE statement
 	'''
-	x=[]
-	i=1
-	while(i<len(p)):
-		x.append(p[i])
-		i=i+1 
-	if len(p) == 6:		
-		p[0] = x #p[1] + p[2] + p[3] + p[4] + p[5]
+	if len(p) == 6:
 		p[0] = ('seleção-decl',p[1], p[2], p[3], p[4], p[5])
 	else:
-		p[0] = x #p[1] + p[2] + p[3] + p[4] + p[5] + p[6] + p[7]
 		p[0] = ('seleção-decl',p[1], p[2], p[3], p[4], p[5], p[6], p[7])
 
 def p_iteracao_decl(p):
@@ -359,118 +286,6 @@ def p_error(p):
 	else:
 		print("Erro sintático na linha %d" % lexer.lineno)
 
-
-
-"""
-def p_id(p):
-	'''
-	id : ID
-	'''
-"""
-# O TRECHO ABAIXO ESTÁ NA DOCUMENTAÇÃO DO PLY
-# ESTAMOS USANDO APENAS PARA TESTES
-
-# Error rule for syntax errors
-# def p_error(p):
-# 	print("Syntax error in input!")
-
-
-# Build the parser
-programa = open('gcd.c-', 'r').read()
-"""
-
-/* COMENTÁRIO */
-int vazio(void){
-	if (1<2){
-		int x;
-		x = 0;
-	}else{
-		return 1;
-	}
-	int y;
-	y = 2/3*4;
-	return 0;
-}
-"""
-"""
-programa = 
-/* Um programa para calcular o mdc
-   segundo o algoritmo de Euclides */  
-    
-   int gcd (int u, int v){
-      if (v==0) return u;
-      else return gcd(u,v,1);
-      /* u-u/v*v == u mod v */
-   }
-"""
-
-lexer = regex.lexer
-
-# Nome do arquivo de entrada do lexer
-parametro = sys.argv[1]
-
-# Abre arquivo e armazena seu conteúdo na variável 'programa'
-arq = open(parametro,'r')
-programa = arq.read();
-
-lexer.input(programa)
 tokens = regex.tokens
-
 parser = yacc.yacc(debug=True)
 
-def printAST(ast):
-	ast=str(ast)
-	ast=ast.split()
-	i = j = flag = 0
-	for word in ast:
-		if('(' in word):
-			print('\n'+ ' '*i +word, end="")
-		else:
-			if(')' in word):
-				i=i-1
-			print(word, end="")
-		'''
-		if('('in word):
-			print('\n'+' '*i+word, end='')
-			flag=1
-		else:
-			print(word+'\n'+(i-1)*' ', end='))')
-		'''
-		i=i+1
-		
-	'''
-	i=0
-	j=i
-	flag=0
-	for word in ast:
-		if('('in word):
-			print('\n'+' '*i+word, end='')
-			flag=1
-		else:
-			if(flag==1):
-				j=0
-				flag=0
-			print((j-1)*' '+word+'\n', end='')
-		i=i+1
-		j=i
-	'''
-
-
-try:
-	teste = parser.parse(programa, lexer=lexer)
-	printAST(teste)
-	print("######################")
-except EOFError:
-	print("EOF")
-	pass
-arq.close();
-# while True:
-# 	try:
-# 		s = input('calc > ')
-# 	except EOFError:
-# 		break
-# 	if not s: continue
-# 	result = parser.parse(s)
-# 	print(result)
-
-# warnings.warn("Warning...........Message")
--- main.py
+++ main.py
@@ -1,46 +1,55 @@
-import regex
-from TabelaSimbolos import *
+import ply.yacc as yacc
 import ply.lex as lex
+import regex
+import glc
+from AST import *
 import sys
+
 
 # Cria lexer com base nas expressões regulares do arquivo regex.py
 # NÃO ativamos o modo 'optmize', pois com ele não podemos identificar qual é a linha
-lexer = lex.lex(module=regex);
+lexer = regex.lexer
 
 # Caso não haja programa de entrada avisa o usuário
 if(len(sys.argv)==1):
 	print('ERROR: Arquivo de entrada não especificado!')
-
+	
 else:
-
-	TS = TabelaSimbolos()
-
 	# Nome do arquivo de entrada do lexer
 	parametro = sys.argv[1]
 
 	# Abre arquivo e armazena seu conteúdo na variável 'programa'
 	arq = open(parametro,'r')
 	programa = arq.read();
-
+	
 	# Lexer recebe programa como entrada
 	lexer.input(programa)
-	 
-	# Enquanto houver conteúdo, lê e tokeniza
-	i=0
-	while True:
-		tok = lexer.token()		
-		if not tok: 
-			break     
-		if(type(tok.value)==list):
-			TS.adiciona(tok.value[0],tok.type,tok.value[1])
-		else:
-			TS.adiciona(tok.value,tok.type,tok.lineno)
-		i=i+1
 
-	print('Número de tokens: '+str(i))
+	# Cria parser com as expressões regulares de glc
+	parser = glc.parser
+
+	try:
+		teste = parser.parse(programa, lexer=lexer)
+		print(teste)
+		#teste = ('raiz',('filho',('neto1','neto2','neto3')))
+		#teste2=('irmao1','irmao2')
+		#ast = criaAST(teste)
+		#printAST(ast)
+		arg1 = []
+		arg2 = []
+		op = []
+		result = []
+		percorre(teste,op,arg1,arg2)
+		#quadPreLoad(op,arg1,arg2,result)
+		#quadSoma(op,arg1,arg2,result)
+		print(op)
+		print(arg1)
+		print(arg2)
+		print(result)
+		
+	except EOFError:
+		print("EOF")
+		pass
 	arq.close();
-	
-	TS.imprime()
-
 
 
